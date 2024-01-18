ef70c3ca86d2d83d10f8ffa166e3e3ef676c2eca
Orange Robotics <orangerobotics@gmail.com>
2024-01-18 17:02:31 -0500

    Created project and began work with Kraken HAL

---
+++ 2024-Robot/src/main/java/frc/robot/subsystems/SwerveModuleIO.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface SwerveModuleIO {
+
+    @AutoLog
+    public class SwerveModuleIOInputs {
+        public double drivePositionMeters = 0.0;
+        public double driveVelocityMetersPerSecond = 0.0;
+        
+        public double angleAbsolutePositionDegrees = 0.0;
+    }
+    
+    /** Updates the set of loggable inputs. */
+    public default void updateInputs(SwerveModuleIOInputs inputs) {};
+
+    /** Run the drive motor at the specified voltage. */
+    public default void setDriveVoltage(double volts) {};
+
+    /** Run the angle motor at the specified voltage. */
+    public default void setAngleVoltage(double volts) {};
+
+}
---
+++ 2024-Robot/src/main/java/frc/robot/subsystems/SwerveModuleIOKraken.java
@@ -0,0 +1,54 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.AbsoluteSensorRangeValue;
+
+public class SwerveModuleIOKraken implements SwerveModuleIO {
+    private double angleOffset;
+    private CANcoder absoluteEncoder;
+    private TalonFX mAngleMotor;
+    private TalonFX mDriveMotor;
+    private TalonFXConfiguration MotorConfigs; // K-stuff in docs ben said no no
+    private CANcoderConfiguration CANConfigs;
+
+    public SwerveModuleIOKraken(int driveMotorID, int angleMotorID, int angleOffset, int cancoderID){
+        this.angleOffset = angleOffset;
+        
+        /* Angle Encoder Config */
+        absoluteEncoder = new CANcoder(cancoderID);
+        configCANCoder();
+        /* Angle Motor Config */
+        mAngleMotor = new TalonFX(angleMotorID);
+
+        /* Drive Motor Config */
+        mDriveMotor = new TalonFX(driveMotorID);
+    }
+
+    @Override
+    public void updateInputs(SwerveModuleIOInputs inputs) {
+        inputs.drivePositionMeters = mDriveMotor.getPosition().getValueAsDouble();
+        inputs.driveVelocityMetersPerSecond = mDriveMotor.getVelocity().getValueAsDouble();
+        inputs.angleAbsolutePositionDegrees = absoluteEncoder.getAbsolutePosition().getValueAsDouble();
+    }
+
+    @Override
+    public void setDriveVoltage(double volts) {
+        mDriveMotor.setVoltage(volts);
+    }
+
+    @Override
+    public void setAngleVoltage(double volts) {
+        mAngleMotor.setVoltage(volts);
+    }
+
+    private void configCANCoder() {
+        CANConfigs = new CANcoderConfiguration(); //idk if this is how youre supposed to do this but its kinda packaged with talonFX sooo
+        CANConfigs.MagnetSensor.AbsoluteSensorRange = AbsoluteSensorRangeValue.Signed_PlusMinusHalf; // last config had [-180, +180) but this only has [-0.5, +0.5) as an option
+        CANConfigs.MagnetSensor.MagnetOffset = angleOffset;
+        absoluteEncoder.getConfigurator().apply(CANConfigs);
+    }
+    
+}
